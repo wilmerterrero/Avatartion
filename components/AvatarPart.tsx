f197ba6874cefb902ec52a5e9f575f4d260e3cf5
Wilmer Terrero <wilmerterrero1922@gmail.com>
2023-03-29 13:54:14 -0400

    fix scale issue

--- components/AvatarPart.tsx
+++ components/AvatarPart.tsx
@@ -7,10 +7,12 @@ type Props = {
 export const AvatarPart = ({ path, scale = 0.2 }: Props) => {
   const Part = require(`~/components/parts/${path}`).default;
 
+  const _scale = `scale-[${scale}]`;
+
   return (
     <div className="w-12 h-12 relative">
       <div
-        className={`absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 scale-[${scale}]`}
+        className={`absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 ${_scale}`}
       >
         <Part />
       </div>
